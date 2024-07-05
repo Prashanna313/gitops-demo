fba182fe13a5c2d7abad6762d6c87386a699ed05
prashanna313 <buskey14@gmail.com>
2024-07-05 07:47:22 +0530

    initial commit

---
+++ main.tf
@@ -0,0 +1,197 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  required_version = ">= 1.9"
+  #   backend "s3" {
+  #     bucket = "gitops-demo-prashanna"
+  #     key = "terraform.tfstate"
+  #     region = "ap-south-1"
+  #   }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+# Create a VPC to launch our instances into
+resource "aws_vpc" "default" {
+  cidr_block = "10.0.0.0/16"
+}
+
+# Create an internet gateway to give our subnet access to the outside world
+resource "aws_internet_gateway" "default" {
+  vpc_id = aws_vpc.default.id
+}
+
+# Grant the VPC internet access on its main route table
+resource "aws_route" "internet_access" {
+  route_table_id         = aws_vpc.default.main_route_table_id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.default.id
+}
+
+# Create a subnet to launch our instances into
+resource "aws_subnet" "default" {
+  vpc_id                  = aws_vpc.default.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = var.aws_availability_zone
+}
+
+# A security group for the ELB so it is accessible via the web
+resource "aws_security_group" "elb" {
+  name        = "terraform_example_elb"
+  description = "Used in the terraform"
+  vpc_id      = aws_vpc.default.id
+
+  # HTTP access from anywhere
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # outbound internet access
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Our default security group to access
+# the instances over SSH and HTTP
+resource "aws_security_group" "default" {
+  name        = "terraform_example"
+  description = "Used in the terraform"
+  vpc_id      = aws_vpc.default.id
+
+  # SSH access from anywhere
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Not recommended to open for all
+  }
+
+  # HTTP access from the VPC
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+
+  # outbound internet access
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_elb" "web" {
+  # name = "terraform-example-elb"
+
+  subnets         = [aws_subnet.default.id]
+  security_groups = [aws_security_group.elb.id]
+  instances       = [aws_instance.web.id, aws_instance.web2.id]
+
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+}
+
+
+resource "aws_instance" "web" {
+  # The connection block tells our provisioner how to
+  # communicate with the resource (instance)
+  connection {
+    type = "ssh"
+    # The default username for our AMI
+    user = "ubuntu"
+    host = self.public_ip
+    # The connection will use the local SSH agent for authentication.
+  }
+  availability_zone = var.aws_availability_zone
+
+  instance_type = "t2.micro"
+
+  # Lookup the correct AMI based on the region
+  # we specified
+  ami = var.aws_amis[var.aws_region]
+
+  # The name of our SSH keypair we created above.
+  key_name = "AwsDevOps"
+
+  # Our Security group to allow HTTP and SSH access
+  vpc_security_group_ids = [aws_security_group.default.id]
+
+  # We're going to launch into the same subnet as our ELB. In a production
+  # environment it's more common to have a separate private subnet for
+  # backend instances.
+  subnet_id = aws_subnet.default.id
+
+  user_data = <<-EOF
+                #!/bin/bash
+                sudo apt update -y
+                sudo apt install apache2 -y
+                sudo systemctl start apache2
+                sudo apt-get install cloud-utils -y
+                EC2_INSTANCE_ID=$(ec2metadata --instance-id)
+                sudo echo "Page is hosted in instance id $EC2_INSTANCE_ID" > /var/www/html/index.html
+                EOF
+
+}
+
+resource "aws_instance" "web2" {
+  # The connection block tells our provisioner how to
+  # communicate with the resource (instance)
+  connection {
+    type = "ssh"
+    # The default username for our AMI
+    user = "ubuntu"
+    host = self.public_ip
+    # The connection will use the local SSH agent for authentication.
+  }
+  availability_zone = var.aws_availability_zone
+
+  instance_type = "t2.micro"
+
+  # Lookup the correct AMI based on the region
+  # we specified
+  ami = var.aws_amis[var.aws_region]
+
+  # The name of our SSH keypair we created above.
+  key_name = "AwsDevOps"
+
+  # Our Security group to allow HTTP and SSH access
+  vpc_security_group_ids = [aws_security_group.default.id]
+
+
+  # We're going to launch into the same subnet as our ELB. In a production
+  # environment it's more common to have a separate private subnet for
+  # backend instances.
+  subnet_id = aws_subnet.default.id
+
+  user_data = <<-EOF
+                #!/bin/bash
+                sudo apt update -y
+                sudo apt install apache2 -y
+                sudo systemctl start apache2
+                sudo apt-get install cloud-utils -y
+                EC2_INSTANCE_ID=$(ec2metadata --instance-id)
+                sudo echo "Page is hosted in instance id $EC2_INSTANCE_ID" > /var/www/html/index.html
+                EOF
+
+}
